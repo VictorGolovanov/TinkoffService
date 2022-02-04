5ab69a935180615b13d9029309d23abde9779b43
Victor Golovanov <vgolovanov10@gmail.com>
2022-02-05 00:32:08 +0300

    initial

---
+++ src/test/java/com/victorgolovanov/TinkoffService/TinkoffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorgolovanov.TinkoffService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
